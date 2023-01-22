d81499584caed27b94f01c4a27c4aea7b7ed36a0
Vasilievkosta <vasilievkosta@gmail.com>
2023-01-22 17:34:33 +0200

    start portfolio project

--- src/App.js
+++ src/App.js
@@ -4,22 +4,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<Header />
+      <h1>My Portfolio</h1>
+	  <Skils />
+	  <Work />
     </div>
   );
+}
+
+function Header() {
+	return (
+		<section className="header">
+		<div className="container">
+			<nav>
+				<ul style={{display:'flex', justifyContent: 'flex-end', listStyleType:'none'}}>
+					<li style={{marginRight:'15px'}}><a href='#' style={{cursor:'pointer'}}>Home</a></li>
+					<li style={{marginRight:'15px'}}><a href='#' style={{cursor:'pointer'}}>Projects</a></li>
+					<li style={{marginRight:'15px'}}><a href='#' style={{cursor:'pointer'}}>Contacts</a></li>
+					<li style={{marginRight:'15px'}}><a href='#' style={{cursor:'pointer'}}>About</a></li>					
+				</ul>
+			</nav>
+		</div>
+		</section>
+	)
+}
+function Skils() {
+	return (
+		<section className="skils" style={{height: '300px', backgroundColor: '#eeffee'}}>
+		<div className="container">
+			<h2>Skils</h2>
+		</div>
+		</section>
+	)
+}
+function Work() {
+	return (
+		<section className="work" style={{height: '300px', backgroundColor: '#c3cdf7'}}>
+		<div className="container">
+			<h2>My Work</h2>
+		</div>
+		</section>
+	)
 }
 
 export default App;
